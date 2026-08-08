8ece49c055b15782d81d59f7c0e28cacfd9bf658
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-08 06:39:57 +0000

    Aug 8, 2026, 6:39 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -7,7 +7,7 @@ function setup() {
 //     rect(width / 2 - rectsize / 2, height / 2 - rectsize / 2, rectsize, rectsize)
 // }
 function keyPressed() {
-    rectsize = 100
+    circle(random(0-400), random(0-400), random(10-25)) 
 }
 function keyReleased() {
     rectsize = 50
