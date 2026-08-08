c71cfff4c4a60ced0c44120c19206481ce1bb175
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-08 06:26:58 +0000

    Aug 8, 2026, 6:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,14 +1,14 @@
-let rectsize = 50
-function setup() {
-    createCanvas(400, 400);
-}
-function draw() {
-    background(220);
-    rect(width / 2 - rectsize / 2, height / 2 - rectsize / 2, rectsize, rectsize)
-}
-function keyPressed() {
-    rectsize = 100
-}
-function keyReleased() {
-    rectsize = 50
-}
+// let rectsize = 50
+// function setup() {
+//     createCanvas(400, 400);
+// }
+// function draw() {
+//     background(220);
+//     rect(width / 2 - rectsize / 2, height / 2 - rectsize / 2, rectsize, rectsize)
+// }
+// function keyPressed() {
+//     rectsize = 100
+// }
+// function keyReleased() {
+//     rectsize = 50
+// }
