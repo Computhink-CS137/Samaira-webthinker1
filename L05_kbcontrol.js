440036deee13b215314d60cd0aa7698f548ec137
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-08 06:44:43 +0000

    Aug 8, 2026, 6:44 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,9 +2,9 @@
 function setup() {
     createCanvas(400, 400);
 }
-// function draw() {
-//     background(220);
-// }
+function draw() {
+    background(220);
+}
 function keyPressed() {
     circle(random(0, 400), random(0, 400), 30)
 }
