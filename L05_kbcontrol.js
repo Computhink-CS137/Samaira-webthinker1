8bcbb7d9f8e4537d28b701ee0ec9a5d2297bc9ca
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-08 07:28:23 +0000

    Aug 8, 2026, 7:28 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -19,7 +19,7 @@ function setup() {
 }
 function keyPressed(){
     console.log("key:", key);
-    console.log("KeyCode:", )
+    console.log("KeyCode:", keyCode)
 
     if(keycode === "48") {
     circle(100, 100, 40)
