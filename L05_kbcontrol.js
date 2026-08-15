4f99f6b238ee0f5132f6bd07649f77fbc25b2041
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-15 06:43:31 +0000

    Aug 15, 2026, 6:43 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -55,12 +55,12 @@ function draw(){
 //     }
 // }
 
-function mouseMove(){
+function mouseMoved(){
     if (mouseX <= 74){
         fill("red");
-    }else if (mouseX >= 75 && mouseX < 125){
+    } else if (mouseX >= 75 && mouseX < 125){
         fill("green");
-    }else{
+    } else{
         fill("blue");
     }
 }
