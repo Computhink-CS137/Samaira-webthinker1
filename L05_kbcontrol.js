6e1f6183a4db3e325f8e9084b9ff229e669acfa0
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-15 07:21:10 +0000

    Aug 15, 2026, 7:21 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -55,7 +55,7 @@ function setup() {
 //     noStroke();
 // }
 function draw() {
-    triangle(x + 25, y + 25, x, y + 25, x + 25, y - 25);
+    triangle(x + 25, y + 25, x, y - 25, x - 25, y + 25);
     if (keyIsDown(up_ARROW)) {
         y -= 1;
     }if (keyIsDown(DOWN_ARROW)) {
