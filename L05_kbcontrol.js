0748675be5705306744c6b3e58c6fd604324dc4e
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-15 06:55:48 +0000

    Aug 15, 2026, 6:55 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -42,7 +42,7 @@ function draw(){
     if(keyIsDown(RIGHT_ARROW)) {
         x += 1;
     }if(keyCode == LEFT_ARROW) {
-        
+        x -= 1;
     }
 
     circle(width / 2 + x, height / 2, 50);
