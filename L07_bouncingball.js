fd4a86bfb5c3a8b6f3f18ef4986a4eb766d1afcf
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 06:58:04 +0000

    Aug 29, 2026, 6:58 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -68,6 +68,6 @@ function draw() {
     // when put 1 number, it your gray scale; 0 = balck, 255 = white
     circle(ballX, ballY, ballSize)
     noStroke();
-    
+    Fill()
     // stroke = outline, fill = color
 }
