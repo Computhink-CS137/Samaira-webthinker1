3cce874d6d4782a19c874fe04b7e4a238b558229
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 06:37:32 +0000

    Aug 29, 2026, 6:37 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -30,7 +30,8 @@ function draw() {
     x = constrain(x, 0, width - 100) 
     y = constrain(y, 0, height - 100)
     // (variable to constrain, 0{top left corner}[min], coordinate of max)
-    // to prevent image from exitting canvas,  
+    // to prevent image from exitting canvas, because the hit box is at the top left corner of the 
+    // image,  
 
     image(picoImage, x, y, 100, 100)
     // (image, x position, y position, image width size, image height size)
