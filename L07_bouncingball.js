dc0edb326e4371763fca08358b15ac125f3f8f9a
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 07:16:27 +0000

    Aug 29, 2026, 7:16 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -85,7 +85,7 @@ function draw() {
         ballSpeedX *= -1
         // flip from positive to negative
         // turing it to negative, because bouncing basically means fliping direction, 
-        // and from positive to nagative you are changing 
+        // and from positive to nagative you are changing direction 
 
     }
      // "||" means "or"
