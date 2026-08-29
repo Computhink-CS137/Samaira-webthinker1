05102f62abc3960c8a92b36dbed10fa56ff676a0
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 06:24:30 +0000

    Aug 29, 2026, 6:24 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -18,10 +18,10 @@ function draw() {
     if(keyIsDown(UP_ARROW)) {
         
     }
-    if(keyIsDown(UP_ARROW)) {
+    if(keyIsDown(RIGHT_ARROW)) {
         
     }
-        if(keyIsDown(UP_ARROW)) {
+    if(keyIsDown(UP_ARROW)) {
         
     }
 
