16ce15edba893ed5fa2a13aa37c78365ea861d57
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 07:20:25 +0000

    Aug 29, 2026, 7:20 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -81,7 +81,7 @@ function draw() {
     // and in each frame it moves 5 pixels. then it will move 300 pixels in 60 seconds.
 
     // bonce on edge
-    if (ballX <= 5 || ballX >= width - 5) {
+    if (ballX <= 0 || ballX >= width - 5) {
              // "||" means "or"
         ballSpeedX *= -1
         // flip from positive to negative
@@ -89,7 +89,7 @@ function draw() {
         // and from positive to nagative you are changing direction 
 
     }
-    if (ballY <= 0 + ball || ballY >= height) {
+    if (ballY <= 0 + ballSize / 2 || ballY >= height) {
         ballSpeedY *= -1
     }
    
