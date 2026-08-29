ebf35995cc97ada774db1a18c6cc5b120b198c93
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 06:23:22 +0000

    Aug 29, 2026, 6:23 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -12,6 +12,8 @@ function setup() {
 function draw() {
     background("gray");
 
+    
+
     image(picoImage, width / 2, height / 2, 100, 100)
     // (image, x position, y position, size, size)
 }
