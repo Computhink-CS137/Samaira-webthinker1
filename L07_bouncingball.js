929e1cf3f0c487ce2ea0e905bed36a53fe0c9dd9
Computhink-CS137 <cs137@computhink.com.sg>
2026-08-29 06:46:22 +0000

    Aug 29, 2026, 6:46 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -46,6 +46,6 @@ function keyPressed() {
     if (keyCode === 32) {
         // can also do (key == "a") of for space key == " "
         popSound.play();
-        picoWidth = 50
+        picoWidth = 50;
     }
 }
