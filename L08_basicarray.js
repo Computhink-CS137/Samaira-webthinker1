942403376763d793ba6142b0f5ed2da3666bbcd0
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-12 06:57:35 +0000

    Sep 12, 2026, 6:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -40,5 +40,6 @@ function setup() {
     text("I am 13 years old", 50, 100)
     text("my favourite activity is reading", 50, 150)
     // array = list in java, each item has an index(number), 
-    // first item in list is index 0
+    // first item in list is index 0 just like python
+    
 }
