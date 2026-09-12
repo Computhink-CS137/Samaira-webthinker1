e482e0c815cc4eeb858e9d658cdf76577a1eca73
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-12 06:41:41 +0000

    Sep 12, 2026, 6:41 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,18 +1,18 @@
-let squareX = 300;
-let squareY = 200;
-let squareSpeed = 5;
-function setup() {
-    createCanvas(600, 400);
-    background(220);
-}
+// let squareX = 300;
+// let squareY = 200;
+// let squareSpeed = 5;
+// function setup() {
+//     createCanvas(600, 400);
+//     background(220);
+// }
 
-function draw() {
-    background(220);
-    // movemet; change square y & x
-    if (squareX > width - 100 || squareX < 0) {
-        squareSpeed *= -1;
-    }
-    squareX += squareSpeed;
-    fill(89, 125, 4,);
-    rect(squareX, squareY, 100, 100)
-}
+// function draw() {
+//     background(220);
+//     // movemet; change square y & x
+//     if (squareX > width - 100 || squareX < 0) {
+//         squareSpeed *= -1;
+//     }
+//     squareX += squareSpeed;
+//     fill(89, 125, 4,);
+//     rect(squareX, squareY, 100, 100)
+// }
