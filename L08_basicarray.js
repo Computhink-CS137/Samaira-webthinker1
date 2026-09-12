e874c38d001088e600ff4473df018ad6aed31ebf
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-12 06:35:00 +0000

    Sep 12, 2026, 6:35 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
     // movemet; change square y & x
-    if (squareX > width) {
+    if (squareX > width || ) {
         squareSpeed *= -1;
     }
     squareX += squareSpeed;
