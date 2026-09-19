edfd00094d3a1361e84062360e7548735d66e1c1
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 06:45:11 +0000

    Sep 19, 2026, 6:45 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -37,7 +37,7 @@ for (let i = 0; i < storyText.length; i++ ) {
 text( storyText[i], width / 2, yPos )
 }
 // string, xPos, yPos
-// storytext[i]
+// storytext[i], display the 
 
 yPos -= 1
 // minus 1 in draw function, so that it repeats each frame 
