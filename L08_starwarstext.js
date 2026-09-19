c0ea3bd748dedbc33f5af09964ea1f9c7950ad6e
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 07:00:48 +0000

    Sep 19, 2026, 7:00 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -46,6 +46,7 @@ for (let i = 0; i < storyText.length; i++ ) {
 // last index of loop = length minus one, because index numbers start from 0
 // thats why start at 0, stop any number before len. = an index number 
 text( storyText[i], width / 4, yPos + i * 30 )
+bgMusic.play()
 }
 // string, xPos, yPos
 // storytext[i], display the i sentence, i = index no.
