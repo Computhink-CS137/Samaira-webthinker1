73f9fdc7ac39dac2165f8cc89824b5f7e763edca
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 07:01:19 +0000

    Sep 19, 2026, 7:01 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -36,6 +36,7 @@ function setup() {
     // yPos later, because want text to move up
     // height is 400, because set canvas like that 
     yPos = height;
+    bgMusic.play();
 }
 
 function draw() {
@@ -46,7 +47,6 @@ for (let i = 0; i < storyText.length; i++ ) {
 // last index of loop = length minus one, because index numbers start from 0
 // thats why start at 0, stop any number before len. = an index number 
 text( storyText[i], width / 4, yPos + i * 30 )
-bgMusic.play();
 }
 // string, xPos, yPos
 // storytext[i], display the i sentence, i = index no.
