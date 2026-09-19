889987b53016efdea8f90239d82e80eb5399801f
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 06:52:09 +0000

    Sep 19, 2026, 6:52 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -38,7 +38,7 @@ text( storyText[i], width / 2, yPos + i * 30 )
 }
 // string, xPos, yPos
 // storytext[i], display the i sentence, i = index no.
-// text hit bot = left side, if aligin ceneter --> left side would be center, 
+// text hit bot = left side, if aligin ceneter --> left side would be center, must do cenet center, or move more to the left.
 
 yPos -= 1
 // minus 1 in draw function, so that it repeats each frame 
