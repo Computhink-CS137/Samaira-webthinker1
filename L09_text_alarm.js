1830ee47a195108cb9994b5120a5a94d83e651c0
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 07:22:49 +0000

    Sep 19, 2026, 7:22 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -17,5 +17,7 @@ function draw() {
     // need to be in draw, so constantly updating, can be in setup because 
     // then it will be syuck on the time you launch
     text(hour, width / 2, height / 2);
+    text(hour, width / 2, height / 2);
+    text(hour, width / 2, height / 2);
 
 }
