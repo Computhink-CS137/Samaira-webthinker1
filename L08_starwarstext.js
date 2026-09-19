f490bf5303be0ed1bbb000f8b2b88e76683916f0
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-19 06:27:49 +0000

    Sep 19, 2026, 6:27 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,19 @@
-// write your codes here
+let yPos;
+function setup() {
+    createCanvas(600, 400);
+    textAlign(LEFT, CENTER);
+    textSize(24);
+    fill("rgb(255, 255, 0)")
+    // set starting height position will change 
+    // yPos later, because want text to move up
+    // height is 400, because set canvas like that 
+    yPos = height;
+}
+
+function draw() {
+  background(0)  
+//   clear canvas
+text("asd", width / 2, yPos )
+yPos -= 1
+// minus 1 in draw function, so that it repeats each frame 
+} 
