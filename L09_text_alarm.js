17ee59da3863e64872f58ed6ec37d58e90bf9132
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 06:50:16 +0000

    Sep 26, 2026, 6:50 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -83,7 +83,7 @@ function updateCountdown() {
     // reduce countdown by 1, evertime it's called, which is every 1 secon
     if (countdown < 1) {
         countdown = 0;
-        
+        clearInterval()
     }
 }
 
