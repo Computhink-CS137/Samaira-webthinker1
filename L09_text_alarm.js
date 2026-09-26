06b87e87b4f2ccca524f0bac9ee13f9582558751
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 06:40:20 +0000

    Sep 26, 2026, 6:40 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -69,7 +69,7 @@ let countdown = 5;
 function setup() {
     createCanvas(600, 400);
     intervalId = setInterval(updateCountdown, 1000);
-    // interval
+    // intervalId = how to identify
     textAlign(CENTER, CENTER);
     textSize(40);
     fill("white");
