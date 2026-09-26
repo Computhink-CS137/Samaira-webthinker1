45f428fd6e688f51793049ef616b090d23ceeeb0
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 07:32:19 +0000

    Sep 26, 2026, 7:32 AM

--- L10_name.js
+++ L10_name.js
@@ -14,7 +14,8 @@ function setup() {
     // create input feild, bos=xes where you click then type/fill in stuff
     // create an are for the user to type, and ans. will be saved in variable
     inputText = createInput();
-    let inputX = this.canvas.offset + (width / 2)
+    let inputX = this.canvas.offset + (width / 2) - 80;
+    let inputY =
     inputText.position(width / 2, height / 2);
 
     // call updateText function when user types
