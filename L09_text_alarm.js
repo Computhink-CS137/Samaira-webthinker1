f65e0c631561946813a95fd45d0c5d2b9ec59701
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 06:57:11 +0000

    Sep 26, 2026, 6:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -95,6 +95,7 @@ function updateCountdown() {
         countdown = 0;
         clearInterval(intervalId)
         // clearing interval = stoping the loop, stop the interval from repeating, with out stopping will go on forver, stop by using the intervalId
+        countdownStart = false;setup;
     }
 }
 
