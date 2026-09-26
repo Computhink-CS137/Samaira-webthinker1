010b583533668bf474254f2fc88e22f20e24a813
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 07:32:30 +0000

    Sep 26, 2026, 7:32 AM

--- L10_name.js
+++ L10_name.js
@@ -15,7 +15,7 @@ function setup() {
     // create an are for the user to type, and ans. will be saved in variable
     inputText = createInput();
     let inputX = this.canvas.offset + (width / 2) - 80;
-    let inputY =
+    let inputY = this.canvas.
     inputText.position(width / 2, height / 2);
 
     // call updateText function when user types
