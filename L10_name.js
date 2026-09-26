3a425c3a2d44492ad1234398c1010ec4408ab7ee
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 07:29:24 +0000

    Sep 26, 2026, 7:29 AM

--- L10_name.js
+++ L10_name.js
@@ -5,7 +5,7 @@ let displayText = "your name; ";
 
 function setup() {
     createCanvas(600, 400);
-    
+
 
     // create input feild, bos=xes where you click then type/fill in stuff
     // create an are for the user to type, and ans. will be saved in variable
@@ -18,6 +18,7 @@ function setup() {
 
 function draw() {
     background(0);
+    text(display)
 }
 
 function updateText() {
