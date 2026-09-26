b3f6aee407a5c3945d0e37c0c07b0c85e40962d4
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 07:16:29 +0000

    Sep 26, 2026, 7:16 AM

--- L10_name.js
+++ L10_name.js
@@ -7,8 +7,8 @@ function setup() {
     createCanvas(600, 400);
 
     // create input feild, bos=xes where you click then type/fill in stuff
-    inputText = crateInput();
     // create an are for the user to type, and ans. will be saved in variable
+    inputText = crateInput();
 }
 
 function draw() {
