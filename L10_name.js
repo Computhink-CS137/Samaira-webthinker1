9373c8afdd83bf4fd67f3da61de5b7e1fb37af75
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 07:31:38 +0000

    Sep 26, 2026, 7:31 AM

--- L10_name.js
+++ L10_name.js
@@ -14,7 +14,7 @@ function setup() {
     // create input feild, bos=xes where you click then type/fill in stuff
     // create an are for the user to type, and ans. will be saved in variable
     inputText = createInput();
-    
+    let inputX = 
     inputText.position(width / 2, height / 2);
 
     // call updateText function when user types
