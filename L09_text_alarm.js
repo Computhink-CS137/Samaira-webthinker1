69a8fad5335d61dc58b6eb476f9b291d95ee1165
Computhink-CS137 <cs137@computhink.com.sg>
2026-09-26 06:56:29 +0000

    Sep 26, 2026, 6:56 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -76,12 +76,14 @@ function setup() {
 } 
 
 function mousepressed() { 
-    if (countdownStart === false) { intervalId = setInterval(updateCountdown, 1000);
+    if (countdownStart === false) 
+        { intervalId = setInterval(updateCountdown, 1000);
     // intervalId = how to identify/call each interval because can have many, is a "fixed" variable
     // every 1000 milliseconds update countdown function will be called
     // everytime click on mouse countdown will resteart becuase code above will run, and 
     // call to updateCountdown
-    }}
+    }
+}
 
 
 function updateCountdown() {
